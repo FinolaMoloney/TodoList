f9712529a8a38fc2794cab9941af08a72270cfd3
Finola Moloney <x22151338@student.ncirl.ie>
2023-04-08 13:05:16 +0100

    added deployment pipeline

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 CURRENT_INSTANCE=$(sudo docker ps -a -q --filter ancestor="$IMAGE_NAME" --format="{{.ID}}")
 
-if [ "$CURRENT_INSTANCE"]
+if [ "$CURRENT_INSTANCE" ]
 then
    sudo docker rm $(sudo docker stop $CURRENT_INSTANCE)
 fi
@@ -9,9 +9,9 @@ fi
 sudo docker pull $IMAGE_NAME
 
 CONTAINER_EXISTS=$(sudo docker ps -a | grep $CONTAINER_NAME)
-if ["$CONTAINER_EXISTS"]
+if [ "$CONTAINER_EXISTS" ]
 then
     sudo docker rm $CONTAINER_NAME
 fi
 
-sudo docker create -p 3000:3000 --name $CONTAINER_NAME $IMAGE_NAME
+sudo docker run -p 3000:3000 -d --name $CONTAINER_NAME $IMAGE_NAME
